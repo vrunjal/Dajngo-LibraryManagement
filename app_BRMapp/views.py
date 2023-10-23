137ba53bd1a208539692a5d345dcab1ea7004110
Vrunjal N. Panchal <vrunjal.panchal2907@gmail.com>
2023-10-23 10:40:04 +0530

    template and view function

--- app_BRMapp/views.py
+++ app_BRMapp/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from app_BRMapp.forms import NewBookForm
+from app_BRMapp.models import Book
+from django.http import HttpResponse
 # Create your views here.
+
+def viewBook(request):
+    books=Book.objects.all()
+
+def newBook(request):
+    form=NewBookForm()
+    return render (request,'app_BMRapp/new_book.html',{'form':form})
+
+def add(request):
+    if request.method=="POST":
+        form=NewBookForm(request.POST)
+        if form.is_valid():
+            nm=form.cleaned_data['title']
+            em=form.cleaned_data['price']
+            pw=form.cleaned_data['author']
+            sw=form.cleaned_data['publisher']
+            
+            reg=Book(title=nm,price=em,author=pw,publisher=sw)
+            reg.save()
+            # fm=NewBookForm()
+    s="Recore Submitted <br><a href='/app_BRMapp/view-books'>View All Books</a>"
+    return HttpResponse(s)
+
+
